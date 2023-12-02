c23371b91d220936b50970d1325e6c1da0a66c08
Roberto Huertas <roberto.huertas@outlook.com>
2023-12-02 11:58:06 +0100

    better tests

--- src/app/day_01.rs
+++ src/app/day_01.rs
@@ -1,15 +1,14 @@
 use actix_web::web;
 
-pub async fn day_01(path: web::Path<String>) -> String {
+pub async fn day_01(path: web::Path<String>) -> Option<String> {
     let value = path
         .into_inner()
         .split('/')
-        .map(|s| s.parse::<i32>().unwrap())
         .take(20)
+        .filter_map(|s| s.parse::<i32>().ok())
         .reduce(|a, b| a ^ b)
         .map(|x| x.pow(3))
-        .map(|x| x.to_string())
-        .unwrap_or_else(|| "0".to_string());
+        .map(|x| x.to_string());
     value
 }
 
@@ -21,15 +20,43 @@ mod tests {
     #[actix_web::test]
     async fn day_01_works() {
         let path = web::Path::from("4/8".to_string());
-        let result = day_01(path).await;
+        let result = day_01(path).await.unwrap();
         assert_eq!(result, "1728");
 
         let path = web::Path::from("10".to_string());
-        let result = day_01(path).await;
+        let result = day_01(path).await.unwrap();
         assert_eq!(result, "1000");
 
         let path = web::Path::from("4/5/8/10".to_string());
-        let result = day_01(path).await;
+        let result = day_01(path).await.unwrap();
         assert_eq!(result, "27");
+
+        // 19 elements
+        let path =
+            web::Path::from("4/5/8/10/12/14/16/18/19/20/4/5/8/10/12/14/16/18/19".to_string());
+        let result = day_01(path).await.unwrap();
+        assert_eq!(result, "8000");
+
+        // 20 elements
+        let path =
+            web::Path::from("4/5/8/10/12/14/16/18/19/20/4/5/8/10/12/14/16/18/19/1100".to_string());
+        let result = day_01(path).await.unwrap();
+        assert_eq!(result, "1375036928");
+
+        // 20+ elements
+        let path = web::Path::from(
+            "4/5/8/10/12/14/16/18/19/20/4/5/8/10/12/14/16/18/19/1100/3230".to_string(),
+        );
+        let result = day_01(path).await.unwrap();
+        assert_eq!(result, "1375036928");
+
+        // wrong path won't take into account wrong elements
+        let path = web::Path::from("4/5/8/10/1".to_string());
+        let result = day_01(path).await.unwrap();
+        assert_eq!(result, "8");
+
+        let path = web::Path::from("4/5/8/10/abc/1".to_string());
+        let result = day_01(path).await.unwrap();
+        assert_eq!(result, "8");
     }
 }
--- tests/day_01.rs
+++ tests/day_01.rs
@@ -12,4 +12,18 @@ async fn day_01_works() {
     assert_request(&service, "/1/4/8", "1728").await;
     assert_request(&service, "/1/10", "1000").await;
     assert_request(&service, "/1/4/5/8/10", "27").await;
+    assert_request(
+        &service,
+        "/1/4/5/8/10/12/14/16/18/19/20/4/5/8/10/12/14/16/18/19/1100",
+        "1375036928",
+    )
+    .await;
+    assert_request(
+        &service,
+        "/1/4/5/8/10/12/14/16/18/19/20/4/5/8/10/12/14/16/18/19/1100/3230",
+        "1375036928",
+    )
+    .await;
+    assert_request(&service, "/1/4/5/8/10/1", "8").await;
+    assert_request(&service, "/1/4/5/8/10/abc/1", "8").await;
 }
